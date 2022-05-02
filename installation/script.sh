7b89b7dc3269723082e1c5c85282f45081d00517
j-beniwal <jeevanbeniwal@gmail.com>
2022-05-02 22:43:48 +0530

    Update script.sh

--- installation/script.sh
+++ installation/script.sh
@@ -16,7 +16,7 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 # install docker engine
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 
 # post installation steps
